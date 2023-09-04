e6ce341ea522da664801e50e3875315b156e1481
NTT1410 <nguyentantru14102002@gmail.com>
2023-09-05 00:55:57 +0700

    modified:   backend-university-entrance/src/main/resources/databases.properties 	modified:   university-entrace/src/App.js 	modified:   university-entrace/src/configs/Apis.js

--- university-entrace/src/App.js
+++ university-entrace/src/App.js
@@ -3,28 +3,38 @@ import Header from "./parts/Header";
 import Footer from "./parts/Footer";
 import NavbarRouter from "./routers/NavbarRouter";
 import { BrowserRouter } from "react-router-dom";
-import { } from "./assets/styles/global.scss";
+import {} from "./assets/styles/global.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import MyUserReducer from "./reducers/MyUserReducer";
 import cookie from "react-cookies";
-
+import HomeAdmin from "./pages/admin/HomeAdmin";
 
 export const MyUserContext = createContext();
 
 const App = () => {
-
-  const [user, dispatch] = useReducer(MyUserReducer, cookie.load("user") || null);
+  const [user, dispatch] = useReducer(
+    MyUserReducer,
+    cookie.load("user") || null
+  );
 
   return (
     <>
-      <MyUserContext.Provider value = {[user,dispatch]}>
+      <MyUserContext.Provider value={[user, dispatch]}>
         <BrowserRouter>
-          <Header />
-          <div id="content">
-            <NavbarRouter />
-          </div>
-          <Footer />
+          {user.userRole.id !== 1 ? (
+            <>
+              <Header />
+              <div id="content">
+                <NavbarRouter />
+              </div>
+              <Footer />
+            </>
+          ) : (
+            <>
+              <HomeAdmin />
+            </>
+          )}
         </BrowserRouter>
       </MyUserContext.Provider>
     </>
