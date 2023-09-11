cf37524993efc4ce36a89375cdbc224dd70e22b6
NgoSaiU <ngosaiu2016@gmail.com>
2023-09-11 08:49:56 +0700

    sáng 11/9

--- university-entrace/src/pages/AdmisNewsComments.js
+++ university-entrace/src/pages/AdmisNewsComments.js
@@ -14,25 +14,25 @@ const AdmisNewsComments = () => {
     const { newsId } = useParams();
 
     const [comments, setComments] = useState(null);
-    const [news, setNews] =  useState(null);
+    const [newsD, setNewsD] = useState(null);
     const [contentComment, setContentComment] = useState();
 
 
     useEffect(() => {
-        const loadNews = async () => {
+        let loadNews = async () => {
             let { data } = await Apis.get(endpoints['newsDetails'](newsId));
-            
-                console.info(data);
-            setNews(data); 
+
+            console.info(data);
+            setNewsD(data);
         }
-        
-        // const loadComments = async () => {
-        //     let {data} = await Apis.get(endpoints['listComments'](newsId));
-            
-        //     console.info(data);
-        //     setComments(data);
-        // }
-        
+
+        let loadComments = async () => {
+            let {data} = await Apis.get(endpoints['listComments'](newsId));
+
+            console.info(data);
+            setComments(data);
+        }
+
         loadNews();
         // loadComments();
 
@@ -53,31 +53,37 @@ const AdmisNewsComments = () => {
 
     // if (news === null || comments === null)
     //     return <MySpinner />;
+    if (newsD === null)
+        return <MySpinner />;
 
     let url = `/Login?next=/admissions/${admissionId}/news/${newsId}`; //quay lai trang hienj tai
 
     return <>
+
         <h1>admissions {admissionId} news {newsId}</h1>
+
         hien thi thong tin chi tiet news va nhung binh luan cua no
         <h1 className="text-center text-info mt-2">CHI TIẾT SẢN PHẨM ({newsId})</h1>
         <Row>
+
             <Col md={5} xs={6}>
-                <h2 className="text-danger">{news.title}</h2>
-                <p>{news.content}</p>  
+                <h2 className="text-danger">{newsD.title}</h2>
+                <p>{newsD.content}</p>
             </Col>
+
         </Row>
         <hr />
 
 
-{/* 
-        {user === null ? <p>Vui lòng <Link to={url}>đăng nhập</Link> để bình luận! </p> : <>
+        
+        {/* {user === null ? <p>Vui lòng <Link to={url}>đăng nhập</Link> để bình luận! </p> : <>
             <Form.Control as="textarea" aria-label="With textarea" value={contentComment} onChange={e => setContentComment(e.target.value)} placeholder="Nội dung bình luận" />
             <Button onClick={addComment} className="mt-2" variant="info">Bình luận</Button>
         </>}
-        <hr />
+        <hr /> */}
 
 
-        <ListGroup>
+        {/* <ListGroup>
             {comments.map(c => <ListGroup.Item id={c.id}>
                 {c.userId.lastName} {c.userId.firstName}- {c.content} - <Moment locale="vi" fromNow>{c.createdDate}</Moment>
             </ListGroup.Item>)
